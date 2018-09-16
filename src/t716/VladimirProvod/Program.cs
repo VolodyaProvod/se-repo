51f8cc4508f5e75becc4f777a2d2ebe73047d8d5
VolodyaProvod <achapovsky@gmail.com>
2018-09-16 21:16:17 +0300

    update

--- src/t716/VladimirProvod/Program.cs
+++ src/t716/VladimirProvod/Program.cs
@@ -6,39 +6,26 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Diagnostics;
 
-namespace ConsoleApp3
+namespace PCLoad
 {
-    class Program
+    class ProgressBar
     {
-        static void FirstType()
-        {
-            Console.WriteLine("   {0,-40}{1,-15}{2,-15}{3,-15}   \n", "Name", "   PID", "     CPU", "         RAM");
-            foreach (Process proc in Process.GetProcesses())
-            {
-                using (PerformanceCounter process = new PerformanceCounter("Process", "% Processor Time", proc.ProcessName))
-                {
-                    process.NextValue();
-                    long gpu = proc.PrivateMemorySize64;
-                    string cpu = proc.ProcessName;
-                    Console.WriteLine("|| {0,-40} | {1,-15} | {2,-15} | {3,-15} ||", proc.ProcessName, proc.Id, Math.Round(process.NextValue()/100,2) + "%", gpu/ 1000000+"Mb");
-                    Thread.Sleep(50);
-                }
-            }
-        }
-        static void SecondType()
+        public void HorizontalPB()
         {
             PerformanceCounter ram = new PerformanceCounter("Memory", "% Committed Bytes In Use");
             Console.Write("[");
             for (int i = 0; i < 50; i++)
             {
-                if (i < ((int)ram.NextValue())/2)
+                if (i < ((int)ram.NextValue()) / 2)
                     Console.Write("|");
                 else
                     Console.Write(".");
             }
-            Console.Write("] (" + Math.Round(ram.NextValue(),2) + "% RAM usage)\n\n");
-            PerformanceCounter cpu = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-            for (int i = 0; i < 11; i++)
+            Console.Write("] (" + Math.Round(ram.NextValue(), 2) + "% RAM usage)\n\n");
+        }
+        public void VerticalPB()
+        {
+            PerformanceCounter cpu = new PerformanceCounter("Processor Information", "% Processor Time", "_Total"); for (int i = 0; i < 9; i++)
             {
                 Console.Write("|");
                 int cpuusage = (int)cpu.NextValue();
@@ -46,7 +33,7 @@ namespace ConsoleApp3
                 {
                     if ((cpuusage / 2) > j)
                     {
-                        Console.Write((char)15);
+                        Console.Write("■");
                     }
                     else
                     {
@@ -58,25 +45,64 @@ namespace ConsoleApp3
                 Thread.Sleep(50);
             }
         }
-
+        public void Table ()
+        {
+            Console.WriteLine("   {0,-40}{1,-15}{2,-15}{3,-15}   \n", "Name", "   PID", "     CPU", "         RAM");
+            foreach (Process proc in Process.GetProcesses())
+            {
+                PerformanceCounter process = new PerformanceCounter("Process", "% Processor Time", proc.ProcessName);
+                long gpu = proc.PrivateMemorySize64;
+                string cpu = proc.ProcessName;
+                process.NextValue();
+                Console.WriteLine("|| {0,-40} | {1,-15} | {2,-15} | {3,-15} ||", cpu, proc.Id, Math.Round(process.NextValue()/100, 2) + "%", gpu / 1000000 + "Mb");
+            }
+        }
+    }
+    class Monitors
+    {
+        public void FirstMonitor()
+        {
+            ProgressBar drawing = new ProgressBar();
+            drawing.Table();
+        }
+        public void SecondMonitor()
+        {
+            
+            ProgressBar drawing = new ProgressBar();
+            drawing.HorizontalPB();
+            drawing.VerticalPB();
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
+            Monitors actions = new Monitors();
             long num = long.Parse(args[0]);
             switch (num)
             {
                 case 1:
-                    Console.Clear();
-                    FirstType();
-                    break;
+                    while (true)
+                    {
+                        Console.Clear();
+                        actions.FirstMonitor();
+                        Thread.Sleep(100);
+                    }
                 case 2:
-                    Console.Clear();
-                    SecondType();
-                    break;
+                    while (true)
+                    {
+                        Console.Clear();
+                        actions.SecondMonitor();
+                        Thread.Sleep(100);
+                    }
                 case 3:
-                    Console.Clear();
-                    FirstType();
-                    SecondType();
-                    break;
+                    while (true)
+                    {
+                        Console.Clear();
+                        actions.FirstMonitor();
+                        actions.SecondMonitor();
+                        Thread.Sleep(10000);
+                    }
                 default:
                     break;
             }
